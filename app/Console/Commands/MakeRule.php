f8b629d7271d8a04aa52b6d7019b822ff3579606
An Phan <an@construct.sg>
2017-02-22 16:20:10 +0800

    Create a make:rule command

---
+++ app/Console/Commands/MakeRule.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeRule extends GeneratorCommand
+{
+/**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Rule';
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:rule {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new checklist rule';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/../stubs/rule.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Rules';
+    }
+}
